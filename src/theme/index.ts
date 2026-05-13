ed5f9a4b5a7a2044ae7e6ee3fc9393e66accc01e
WiyarAhmadZai <mrwiyarahmadzai@gmail.com>
2026-05-14 00:42:23 +0430

    updated

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,10 +1,15 @@
+import { animation } from './animations';
 import { colors } from './colors';
 import { radius } from './radius';
+import { shadow } from './shadows';
 import { spacing } from './spacing';
 import { typography } from './typography';
 
+export * from './animations';
 export * from './colors';
+export * from './helpers';
 export * from './radius';
+export * from './shadows';
 export * from './spacing';
 export * from './typography';
 
@@ -13,6 +18,8 @@ export const theme = {
   spacing,
   typography,
   radius,
+  shadow,
+  animation,
 } as const;
 
 export type Theme = typeof theme;
